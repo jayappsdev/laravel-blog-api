3d4c4101bd5a2c52514962ecfe6edc29b2d389ed
jayappsdev <jayappsdev@gmail.com>
2024-01-17 05:23:57 +0100

    refactor_routes_commit

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\BlogController;
-use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CommentController;
 
 
 /*
